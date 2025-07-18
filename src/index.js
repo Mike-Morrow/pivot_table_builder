6f5142f7ad6c75e7895bb45f9ebfe933bd276965
Mike Morrow <mgmorrow8@gmail.com>
2025-07-18 13:38:22 -0400

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import PivotTableBuilder from './PivotTableBuilder';
+import './index.css';  // optional, for basic styles or Tailwind
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<PivotTableBuilder />);
